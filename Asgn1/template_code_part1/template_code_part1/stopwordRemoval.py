dddbfb126b168bb9804eaf9d3157483625c284ef
Nihal John George <nihalgeorge01@gmail.com>
2022-02-24 00:20:24 +0530

    Add stopword removal

--- Asgn1/template_code_part1/template_code_part1/stopwordRemoval.py
+++ Asgn1/template_code_part1/template_code_part1/stopwordRemoval.py
@@ -1,15 +1,19 @@
 from util import *
 
 # Add your import statements here
+import nltk
+nltk.download('stopwords')
 
-
-
+from nltk.corpus import stopwords
 
 class StopwordRemoval():
 
+	def __init__(self):
+		self.sw_set = set(stopwords.words('english'))	# Use set for fast membership check
+	
 	def fromList(self, text):
 		"""
-		Sentence Segmentation using the Punkt Tokenizer
+		Stopword removal using NLTK Stopwords collection
 
 		Parameters
 		----------
@@ -28,9 +32,20 @@ class StopwordRemoval():
 
 		#Fill in code here
 
+		stopwordRemovedText = []
+		for sent in text:
+			new_sent = []
+			for token in sent:
+				if token not in self.sw_set:
+					new_sent.append(token)
+			stopwordRemovedText.append(new_sent)
+
 		return stopwordRemovedText
 
-
+if __name__ == '__main__':
+	# Testing
+	swr = StopwordRemoval()
+	print(swr.fromList([['i', 'like', 'myself']]))
 
 
 	
