e7ed1e5e59d85b51d4d20221c56fbf2a7c161ccb
Nihal John George <nihalgeorge01@gmail.com>
2022-02-24 11:05:52 +0530

    Add stemming and lemmatization

--- Asgn1/template_code_part1/template_code_part1/inflectionReduction.py
+++ Asgn1/template_code_part1/template_code_part1/inflectionReduction.py
@@ -2,10 +2,14 @@ from util import *
 
 # Add your import statements here
 
-
-
+import nltk
+nltk.download('wordnet')
 
 class InflectionReduction:
+
+	def __init__(self):
+		self.stemmer = nltk.stem.SnowballStemmer('english')
+		self.lemmatizer = nltk.stem.WordNetLemmatizer()
 
 	def reduce(self, text):
 		"""
@@ -23,11 +27,59 @@ class InflectionReduction:
 			A list of lists where each sub-list is a sequence of
 			stemmed/lemmatized tokens representing a sentence
 		"""
+		
+		isStem = True
+		
+		if isStem:
+			reducedText = self.stem(text)
+		else:
+			reducedText = self.lemmatize(text)
 
-		reducedText = None
+		return reducedText
 
-		#Fill in code here
+	def stem(self, text):
+		"""
+		Stemming using the Snowball stemmer from NLTK
+		https://stackoverflow.com/questions/24647400/what-is-the-best-stemming-method-in-python
+		
+		Parameters
+		----------
+		arg1 : list
+			A list of lists where each sub-list a sequence of tokens
+			representing a sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of
+			stemmed tokens representing a sentence
+		"""
+		
+		reducedText = [[self.stemmer.stem(token) for token in sent] for sent in text]
 		
 		return reducedText
 
+	def lemmatize(self, text):
+		"""
+		Lemmatization using Wordnet from NLTK
+		
+		Parameters
+		----------
+		arg1 : list
+			A list of lists where each sub-list a sequence of tokens
+			representing a sentence
 
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of
+			lemmatized tokens representing a sentence
+		"""
+		
+		reducedText = [[self.lemmatizer.lemmatize(token) for token in sent] for sent in text]
+		
+		return reducedText
+
+if __name__=='__main__':
+	ir = InflectionReduction()
+	print(ir.lemmatize([['cars', 'suitcases']]))
